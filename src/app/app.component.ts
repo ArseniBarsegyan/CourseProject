f95ff0659dbc7f93045568059f378f1c2d9c09f4
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2018-07-17 00:07:20 +0300

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import {Component, OnInit} from '@angular/core';
+import * as firebase from 'firebase';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'app';
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyD2VnhmHEC3DwfasPvPyrwHyDOJiM8762w',
+      authDomain: 'ng-recipe-book-c8210.firebaseapp.com'
+    });
+  }
+}
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,54 @@
+import { Subject } from 'rxjs/Subject';
+
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+
+export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
+
+  private recipes: Recipe[] = [
+    new Recipe('A test recipe', 'this is simply a test',
+      'http://del.h-cdn.co/assets/17/34/980x490/landscape-1503418862-chicken-thighs-delish.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe( 'Nuka-Cola', 'from wasteland',
+      'http://i0.kym-cdn.com/photos/images/original/000/814/409/f24.jpg',
+      [
+        new Ingredient('Empty bottle', 1),
+        new Ingredient('agave', 2),
+        new Ingredient('Gloving element', 1)
+      ])
+  ];
+
+  constructor() {}
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+}
