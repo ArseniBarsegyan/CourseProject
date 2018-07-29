4d6ed33ba6543486bda05571dd0e7007f040f42d
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2018-07-29 17:31:33 +0300

    Add more actions to shopping-list.actions, reducers update.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,7 +5,6 @@ import {SharedModule} from '../shared/shared.module';
 import {AppRoutingModule} from '../app-routing.module';
 import {DataStorageService} from '../shared/data-storage.service';
 import {AuthService} from '../auth/auth.service';
-import {ShoppingListService} from '../shopping-list/shopping-list.service';
 import {RecipeService} from '../recipes/recipe.service';
 import {HTTP_INTERCEPTORS} from '@angular/common/http';
 import {AuthInterceptor} from '../shared/auth.interceptor';
@@ -25,7 +24,6 @@ import {LoggingInterceptor} from '../shared/logging.interceptor';
     HeaderComponent
   ],
   providers: [
-    ShoppingListService,
     RecipeService,
     DataStorageService,
     AuthService,
