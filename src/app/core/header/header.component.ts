3d0ac3653a78d343baa20a8bef0f334662f1209c
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2018-09-25 22:54:22 +0300

    Code refactor: auth service deleted. Using store directly

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,11 +1,11 @@
 import {Component, OnInit} from '@angular/core';
 
 import { DataStorageService } from '../../shared/data-storage.service';
-import { AuthService } from '../../auth/auth.service';
 import {Store} from '@ngrx/store';
 import * as fromApp from '../../store/app.reducers';
 import * as fromAuth from '../../auth/store/auth.reducers';
 import {Observable} from 'rxjs/index';
+import * as authActions from '../../auth/store/auth.actions';
 
 @Component({
   selector: 'app-header',
@@ -15,7 +15,6 @@ export class HeaderComponent implements OnInit {
   authState: Observable<fromAuth.State>;
 
   constructor(private dataStorageService: DataStorageService,
-              private authService: AuthService,
               private store: Store<fromApp.AppState>) {
   }
 
@@ -37,6 +36,6 @@ export class HeaderComponent implements OnInit {
   }
 
   onLogout() {
-    this.authService.logout();
+    this.store.dispatch(new authActions.Logout());
   }
 }
