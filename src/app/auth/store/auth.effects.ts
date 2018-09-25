90c98da89340836c7b7d9d79019d374e173340d1
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2018-09-25 22:46:14 +0300

    auth effects: added try sign up and try sign in effects

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -1,5 +1,9 @@
 import {Actions, Effect} from '@ngrx/effects';
 import {Injectable} from '@angular/core';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/switchMap';
+import { fromPromise } from 'rxjs/observable/fromPromise';
+import * as firebase from 'firebase';
 
 import * as AuthActions from './auth.actions';
 
@@ -7,7 +11,47 @@ import * as AuthActions from './auth.actions';
 export class AuthEffects {
   @Effect()
   authSignup = this.actions$
-    .ofType(AuthActions.TRY_SIGNUP);
+    .ofType(AuthActions.TRY_SIGNUP)
+    .map((action: AuthActions.TrySignup) => {
+      return action.payload;
+    })
+    .switchMap((authData: {username: string, password: string}) => {
+      return fromPromise(firebase.auth().createUserWithEmailAndPassword(authData.username, authData.password));
+    })
+    .switchMap(() => {
+      return fromPromise(firebase.auth().currentUser.getIdToken());
+    })
+    .mergeMap((token: string) => {
+      return [
+        {type: AuthActions.SIGNUP},
+        {
+          type: AuthActions.SET_TOKEN,
+          payload: token
+        }
+      ];
+    });
+
+  @Effect()
+  authSignin = this.actions$
+    .ofType(AuthActions.TRY_SIGNIN)
+    .map((action: AuthActions.TrySignin) => {
+      return action.payload;
+    })
+    .switchMap((authData: {username: string, password: string}) => {
+      return fromPromise(firebase.auth().signInWithEmailAndPassword(authData.username, authData.password));
+    })
+    .switchMap(() => {
+      return fromPromise(firebase.auth().currentUser.getIdToken());
+    })
+    .mergeMap((token: string) => {
+      return [
+        {type: AuthActions.SIGNIN},
+        {
+          type: AuthActions.SET_TOKEN,
+          payload: token
+        }
+      ];
+    });
 
   constructor(private actions$: Actions) {
   }
